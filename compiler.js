cdf2415adcad8334d3190f239070dc1ad227c2c0
StioStudio <simon.s.johanssen@gmail.com>
2023-01-17 19:39:05 +0100

    update

--- compiler.js
+++ compiler.js
@@ -1,23 +1,51 @@
 
-let stio = {
-    
-    files: [],
-
-    async upload(_file) {
-        let stio_file = fetch(_file)
-            .then(Response => {
-                return Response;
-            }).then(Response => {
-                return Response.text()
-            })
-
-        let rem;
-        await stio_file.then(e => {
-            rem = e
-        })
-        return rem
-    },
-
+/* compiler code. DO NOT USE!*/
+let comp = {
+    take_away_comments(_file) {
+        let rem = 
+`//helloworld
+say("hello world") //hi
+function say (msg) {
+    console.log(msg)
 }
 
-export { Install_Compiler }
+`
+        console.log(rem);
+        
+    },
+    /**Gives an array of each line
+     */
+    split_with_next_line(_file) {
+        return files[0].split("\r\n");
+    }
+}
+
+let files = [];
+
+/**Gives you an string of the file you specified
+ */
+async function download(_file) {
+    let stio_file = fetch(_file)
+    .then(Response => {
+        return Response;
+    }).then(Response => {
+        return Response.text()
+    })
+    
+    let rem;
+    await stio_file.then(e => {
+        rem = e
+    })
+    return rem
+}
+
+/**Runs the code as an stio file
+ */
+function run(_file) {
+    let rem;
+    rem = comp.split_with_next_line(_file)
+    for (let i = 0; i < rem.length; i++) {
+        console.log(rem[i])
+    }
+    rem = comp.take_away_comments(_file)
+}
--- setup.js
+++ setup.js
@@ -1,15 +1,4 @@
-import { Install_Compiler } from "./compiler.js"
-await eval(Install_Compiler("./compiler.js"))
 
-await stio.upload("./code.stio")
-
-let out = stio.files[0].split("\r\n");
-
-for (var i = 0; i < out.length; i++) {
-    if (!out[i].startsWith("//"))
-        console.log(out[i]);
-    else
-        continue
-}
-
+files.push(await download("./code.stio"))
+run(files[0])
 
